1ce31ae9671100d36244551cec3f9fc7a9947da7
Kenshin_Tu <tunx406.ktl@gmail.com>
2018-08-07 15:52:49 +0700

    [M] Update source

--- CMS-Web/Controllers/ShopController.cs
+++ CMS-Web/Controllers/ShopController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CMS_DTO.CMSProduct;
+using CMS_Shared;
+using CMS_Shared.CMSBrands;
+using CMS_Shared.CMSCategories;
+using CMS_Shared.CMSProducts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +11,59 @@ using System.Web.Mvc;
 
 namespace CMS_Web.Controllers
 {
-    public class ShopController : Controller
+    public class ShopController : HQController
     {
+        private CMSProductFactory _fac;
+        private CMSCategoriesFactory _facCate;
+        private CMSBrandsFactory _facBrand;
+        public ShopController()
+        {
+            _fac = new CMSProductFactory();
+            _facCate = new CMSCategoriesFactory();
+            _facBrand = new CMSBrandsFactory();
+            ViewBag.Range = GetListRangeSelectItem();
+        }
         // GET: Shop
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                ProductViewModels model = new ProductViewModels();
+                //Category
+                model.ListCate = _facCate.GetList().OrderByDescending(x => x.CreatedDate).Skip(0).Take(10).ToList();
+                //Category
+                model.ListBrand = _facBrand.GetList().OrderByDescending(x => x.CreatedDate).Skip(0).Take(5).ToList();
+                //Product
+                model.ListProduct = _fac.GetList().OrderByDescending(x => x.CreatedDate).ToList();
+                var dataImage = _fac.GetListImage();
+                if (model.ListProduct != null && model.ListProduct.Any())
+                {
+                    model.ListProduct.ForEach(x =>
+                    {
+                        var _Image = dataImage.FirstOrDefault(z => z.ProductId.Equals(x.Id));
+                        if (_Image != null)
+                        {
+                            x.ImageURL = _Image.ImageURL;
+                            if (!string.IsNullOrEmpty(x.ImageURL))
+                            {
+                                x.ImageURL = Commons._PublicImages + "Products/" + x.ImageURL;
+                            }
+                            else
+                            {
+                                x.ImageURL = "";
+                            }
+                        }
+                    });
+                    model.ListProduct = model.ListProduct.Skip(0).Take(12).ToList();
+                    model.ListProductTopSales = model.ListProduct.Skip(0).Take(5).ToList();
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                // NSLog.Logger.Error("Index: ", ex);
+                return new HttpStatusCodeResult(400, ex.Message);
+            }
         }
     }
 }
