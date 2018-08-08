0ce6e427d97df2bed09956b1f1954a6a37b42692
TuanVo <voquoctuanqn@gmail.com>
2018-08-08 17:07:59 +0700

    [M] update source

--- CMS-Web/Controllers/CollectionController.cs
+++ CMS-Web/Controllers/CollectionController.cs
@@ -17,7 +17,7 @@ namespace CMS_Web.Controllers
         private CMSProductFactory _fac;
         private CMSCategoriesFactory _facCate;
         private CMSBrandsFactory _facBrand;
-
+        private int PageSize = 2;
         public CollectionController()
         {
             _fac = new CMSProductFactory();
@@ -79,7 +79,12 @@ namespace CMS_Web.Controllers
                             }
                         }
                     });
-                    model.ListProduct = model.ListProduct.Where(o=>o.CategoryId.Equals(id)).Skip(0).Take(12).ToList();
+                    var TotalProduct = model.ListProduct.Count(o => o.CategoryId.Equals(id));
+                    if (TotalProduct % PageSize == 0)
+                        model.TotalPage = TotalProduct / PageSize;
+                    else
+                        model.TotalPage = Convert.ToInt32(TotalProduct / PageSize) + 1;
+                    model.ListProduct = model.ListProduct.Where(o=>o.CategoryId.Equals(id)).Skip(0).Take(PageSize).ToList();
                     model.ListProductTopSales = model.ListProduct.Skip(0).Take(5).ToList();
                 }
                 if (!string.IsNullOrEmpty(id))
@@ -109,6 +114,42 @@ namespace CMS_Web.Controllers
                 //NSLog.Logger.Error("GetDetail: ", ex);
                 return new HttpStatusCodeResult(400, ex.Message);
             }
+        }
+
+        public ActionResult LoadDataPagging(int pageIndex,string cateId)
+        {
+            ProductViewModels model = new ProductViewModels();
+            try
+            {
+                //Product
+                model.ListProduct = _fac.GetList().Where(o => o.CategoryId.Equals(cateId)).OrderByDescending(x => x.CreatedDate).ToList();
+                var dataImage = _fac.GetListImage();
+                if (model.ListProduct != null && model.ListProduct.Any())
+                {
+                    model.ListProduct.ForEach(x =>
+                    {
+                        var _Image = dataImage.FirstOrDefault(z => z.ProductId.Equals(x.Id));
+                        if (_Image != null)
+                        {
+                            x.ImageURL = _Image.ImageURL;
+                            if (!string.IsNullOrEmpty(x.ImageURL))
+                            {
+                                x.ImageURL = Commons._PublicImages + "Products/" + x.ImageURL;
+                            }
+                            else
+                            {
+                                x.ImageURL = "";
+                            }
+                        }
+                    });
+                    model.ListProduct = model.ListProduct.Skip((pageIndex - 1) * PageSize).Take(PageSize).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                NSLog.Logger.Error("LoadDataPagging :", ex);
+            }
+            return PartialView("_ListItem", model);
         }
     }
 }
